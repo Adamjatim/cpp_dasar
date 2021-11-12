c67a668793ae0b57ebe2bdf75a82fa6e05779061
adamjatim <adamrahmatilahi@gmail.com>
2021-11-12 13:48:22 +0700

    commit

--- kuliah/bu_sulis/week_04/soal_06/soal_06.cpp
+++ kuliah/bu_sulis/week_04/soal_06/soal_06.cpp
@@ -4,14 +4,13 @@
 // soal :
 //          Hitunglah luas daerah yang diarsir (*) berikut:
 //
-//           ###############
-//           #****** ******#
-//           #*****   *****#
-//           #****     ****#
-//           #***       ***#
-//           #**         **#
-//           #*           *#
-//           ###############
+//           #############
+//           #***** *****#
+//           #****   ****#
+//           #***     ***#
+//           #**       **#
+//           #*         *#
+//           #############
 //       
 //          panjang persegi : 10 cm
 //          lebar persegi : 20 cm
@@ -20,9 +19,26 @@
 int main(void){
     int panjang, lebar, luas_persegi;
     float luas_segitga, luas_arsir;
+    
     std::cout << "Program penghitung luas daerah yang diarsir" 
     << std::endl << std::endl 
-    << "###############" << std::endl
-    << "#****** ******#" << std::endl
+    << "#############" << std::endl
+    << "#***** *****#" << std::endl
+    << "#****   ****#" << std::endl
+    << "#***     ***#" << std::endl
+    << "#**       **#" << std::endl
+    << "#*         *#" << std::endl
+    << "#############" << std::endl;
+    panjang = 10;
+    lebar = 20;
+    std::cout << "Panjang persegi : " << panjang << std::endl;
+    std::cout << "Lebar persegi   : " << lebar << std::endl;
     
+    // rumus
+    luas_persegi = panjang * lebar;
+    luas_segitga = 0.5 * panjang * lebar;
+    luas_arsir = luas_persegi - luas_segitga;
+
+    std::cout << "Luas yang diarsir bernilai : " << luas_arsir << std::endl;
+    std::cout << std::endl << std::endl;
 }
